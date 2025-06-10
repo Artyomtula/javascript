b6f6bee62f8fda733ac84b48ff761a8bebadab65
Artyom <artyomtyulyakov@gmail.com>
2025-06-10 10:44:57 +0300

    fix bugs

--- Lab4/lab4.js
+++ Lab4/lab4.js
@@ -56,9 +56,9 @@ class Book {
 }
 
 function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear()).slice(-2);
   return `${day}.${month}.${year}`;
 }
 
